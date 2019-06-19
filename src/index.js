6e1ecefd4a722a823a27da4dc45af11c4f2e9a4c
Souma Ghosh <souma.ghosh@mountblue.io>
2019-06-19 10:27:23 +0530

    Add Delete and Update functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-let config = fetch('./config.js');
+let key = 'a71a2a6e920d2c4de0ff6a3c977dfbca';
+let token = 'fcb04beb183631337d2659876641a6c88482102930147d398e30b87b9f1d9003';
+
+// let config = fetch('./config.js');
 let trello = fetch('./trello');
-//  import config from './config';
-// import trello from './trello';
+// let config = require('./config.js');
+// let trello = require('./trello');
 
-const listsUrl = 'https://api.trello.com/1/boards/1DOPhXcj/lists?key=' + config.key + '&token=' + config.token;
+const listsUrl = 'https://api.trello.com/1/boards/1DOPhXcj/lists?key=' + key + '&token=' + token;
 
 trello.getId(listsUrl, 'Project 4')
-.then(listId => trello.getCards(listId, config.key, config.token))
+.then(listId => trello.getCards(listId, key, token))
 .then(cards => trello.getAllChecklistsIds(cards))
-.then(checkListIds => trello.getAllCheckItems(checkListIds, config.key, config.token))
+.then(checkListData => trello.getAllCheckItems(checkListData, key, token))
+// .then(checkItemsArray => trello.)
 .catch(error => console.log(error))
 
+console.log(key);
--- src/trello.js
+++ src/trello.js
@@ -1,11 +1,17 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 let get = function(url) {
     return fetch(url).then(response => response.json());
 }
-module.exports.get = get;
+let put = function(url) {
+    return fetch(url, {method: 'PUT'})
+}
+let del = function(url) {
+    return fetch(url, {method: 'delete'})
+}
+// module.exports.get = get;
 
-module.exports.getId = async function (url, name) {
+let getId = async function (url, name) {
     let jsonData = await get(url);
     let id = jsonData.reduce((acc, currVal) => {
         if (currVal.name === name) {
@@ -13,64 +19,141 @@ module.exports.getId = async function (url, name) {
         }
         return acc;
     }, '');
-    // console.log(id);
     return id;
 }
+// module.exports.getId = getId;
 
-module.exports.getCards = async function (listId, key, token) {
+let getCards = async function (listId, key, token) {
     let listUrl = 'https://api.trello.com/1/lists/' + listId + '/cards?key=' + key + '&token=' + token;
     let cards = await get(listUrl);
-    // console.log(cardsJsonData)
     return cards;
 }
+// module.exports.getCards = getCards
 
-module.exports.getAllChecklistsIds = function(cardsJsonData) {
-    let checklistIds = cardsJsonData.reduce((acc, currVal) => {
-        acc.push(...(currVal.idChecklists))
+let getAllCardIds = function(cardsJsonData) {
+    // console.log(cardsJsonData);
+    
+    let cardIds = cardsJsonData.reduce((acc, currCard) => {
+        acc.push(currCard.id);
         return acc
     }, [])
-    return checklistIds;
+    return cardIds;
+}
+// module.exports.getAllChecklistsIds = getAllChecklistsIds;
+
+function getCheckLists(cardId, key, token) {
+    let checkListsUrl = `https://api.trello.com/1/cards/${cardId}/checklists?key=${key}&token=${token}`;
+    return get(checkListsUrl);
 }
 
-function getCheckItems(checklistId, key, token) {
-    let checkListUrl = `https://api.trello.com/1/checklists/${checklistId}/checkItems?key=${key}&token=${token}`;
-    return get(checkListUrl);
-}
-
-module.exports.getAllCheckItems = async function(checklistIds, key, token) {
-    let promiseArray = [];
-    for(let i = 0; i < checklistIds.length; i++) {
-        promiseArray[i] = getCheckItems(checklistIds[i], key, token);
+let getAllCheckItems = async function(cardIds, key, token) {
+    let checklistArray = [];
+    for(let i = 0; i < cardIds.length; i++) {
+        checklistArray[i] = await getCheckLists(cardIds[i], key, token);
     }
-    // console.log(promiseArray);
-    
-    let checkItemsArray = await Promise.all(promiseArray)
-    .then(promises => {
-        return spreadItems(promises);
-    })
-    .catch(error => console.log('Promise.all not resolving', error));
-    // console.log(checkItemsArray);
-    let data= checkItemsArray.map(item=>item.name);
-    return getCheckData(data);
-}
-
-function spreadItems(nestedData) {
-    let result = nestedData.reduce((checkItems, currCheckList) => {
-        // let checkItemsOfOneList = [];
-        currCheckList.forEach(element => {
-            let checkItem = {
-                name: element.name,
-                state: element.state 
-            }
-            checkItems.push(checkItem);
+    let checkItemsArray = checklistArray.reduce((checkItems, checklist, index) => {
+        let cardId = cardIds[index];
+        checklist.forEach(element => {   
+            checklist[0].checkItems.forEach(element => {
+                let checkItem = {
+                    "cardId": cardId,
+                    "id": element.id,
+                    name: element.name,
+                    state: element.state  
+                };
+                checkItems.push(checkItem);
+            })
         });
-        // checkItems.push(checkItemsOfOneList);
         return checkItems;
     }, [])
-    return result;
+    return checkItemsArray;
 }
-function getCheckData(data){
+// module.exports.getAllCheckItems = getAllCheckItems;
 
-let item=`<li>${data}</li>`
-    return data.map(item=>document.getElementById('trello-items').append(item));
+let buildContent = function (checkItemsArray) {
+    var myDiv = document.getElementById("list"); 
+    
+    let complete = document.createElement('style');
+    complete.innerHTML = `.complete{text-decoration: line-through #D3D3D3;}`
+    checkItemsArray.forEach(element => {
+        // console.log(element);
+        let p = document.createElement('p');
+        p.id = 'a' + element.id;
+        // p.class = 'checkItem'
+        p.innerHTML = `<input type=checkbox class = ${element.cardId} id = a${element.id}><label>${element.name}</label><span class = ${element.cardId} id=a${element.id}>&#10005;</span><br>`;
+        myDiv.appendChild(p);
+        // let 
+        if(element.state === 'complete') {
+            document.querySelector(`#a${element.id}`).style.cssText = 'color: #d3d3d3; text-decoration: line-through #D3D3D3;';
+            document.querySelector(`#a${element.id} #a${element.id}`).checked = true;
+        } else {
+            document.querySelector(`#a${element.id}`).style.cssText = 'color: black; text-decoration: none;';
+            document.querySelector(`#a${element.id} #a${element.id}`).checked = false;
+        }
+    })
+
+    let addButton = document.createElement('p');
+    addButton.class = 'button';
+    addButton.innerHTML = '<input type=button value=&#10010>';
+    addButton.addEventListener('click', (event => add(event)));
+    myDiv.appendChild(addButton);
+    
+    // console.log(addButton);
+
+    myDiv.addEventListener('change', (event) => update(event));
+    myDiv.addEventListener('click', (event) => remove(event));
+    document.body.appendChild(myDiv);
+    document.body.style.cssText = 'background-color: #1e90ff;';
+    myDiv.style.cssText = 'background-color: white; width: 35%; margin:auto; padding: 2%'    
+    // document.getElementsByClassName('.checkItem').forEach(element => element.style.cssText = 'cursor: pointer')
+    document.querySelectorAll('span').forEach(element => element.style.cssText = 'float: right; cursor: pointer')
 }
+
+async function update(event) {
+    console.log(event.target.id);
+    let itemId = (event.target.id).slice(1);
+    ((event.target).checked) ? (state = 'complete') : (state = 'incomplete');
+    updateUrl = `https://api.trello.com/1/cards/${event.target.className}/checkItem/${itemId}?state=${state}&key=${key}&token=${token}`
+    await put(updateUrl);
+    // console.log(document.querySelector(`#${event.target.id}`));
+    if(state === 'complete') {
+        document.querySelector(`#${event.target.id}`).style.cssText = 'color: #d3d3d3; text-decoration: line-through #D3D3D3;';
+    } 
+    if(state === 'incomplete') {
+        document.querySelector(`#${event.target.id}`).style.cssText = 'color: black; text-decoration: none;';
+    }
+}
+
+async function remove(event) {
+    if(event.target.nodeName.toLowerCase() === 'span') {
+        let itemId = (event.target.id).slice(1);
+        console.log(itemId);
+        deleteUrl = `https://api.trello.com/1/cards/${event.target.className}/checkItem/${itemId}?key=${key}&token=${token}`;
+        await del(deleteUrl);
+        document.getElementById(event.target.id).remove();
+    }
+} 
+
+async function add(event) {
+    console.log(event.target.parentElement);
+    event.target.parentElement
+    console.log(document.querySelector('#list').lastChild)
+}
+
+
+// module.exports.addItems = addItems;
+
+
+// Calling all functions below
+
+let key = 'a71a2a6e920d2c4de0ff6a3c977dfbca';
+let token = 'fcb04beb183631337d2659876641a6c88482102930147d398e30b87b9f1d9003';
+
+const listsUrl = 'https://api.trello.com/1/boards/1DOPhXcj/lists?key=' + key + '&token=' + token;
+
+getId(listsUrl, 'Project 4')
+.then(listId => getCards(listId, key, token))
+.then(cards => getAllCardIds(cards))
+.then(checkListIds => getAllCheckItems(checkListIds, key, token))
+.then(checkItemsArray => buildContent(checkItemsArray))
+.catch(error => console.log(error))
